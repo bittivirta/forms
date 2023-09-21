f18f8f327d712127ed17f1154c0b7228ae39d091
Mikko Salonen <mikko.salonen@tuni.fi>
2023-09-21 17:45:58 +0300

    Added temp index page.

---
+++ app/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Col, Container, Row } from "@/app/components/bootstrap";
+export default function index() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1 className="display-1">Home</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
