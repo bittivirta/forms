d0986c3735a30d1386da79184adfc371d0fff3f5
Mikko Salonen <mikko.salonen@tuni.fi>
2023-10-07 18:48:15 +0300

    Changed ShowForm function to only accept JSON values.

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -16,22 +16,20 @@ const form = {
     ["submit", "submit", "Submit", "Submit", true]
   ]
 }
-
-
-function ShowForm(json) {
+function ShowForm(form: JSON) {
   // parse json and create form with included fields.
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
       <div className="mx-auto max-w-2xl">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          {json.title}
+          {form.title}
         </h1>
         <p className="text-xl text-gray-600 dark:text-gray-400">
-          {json.description}
+          {form.description}
         </p>
       </div>
       <div>
-        {json.fields.map((field, i) => (
+        {form.fields.map((field, i) => (
           <form key={i}>
           <div className="rounded-lg bg-primary-100 p-4 text-primary-600 dark:bg-primary-900 dark:text-primary-300">
             <h2 className="text-3xl font-extrabold leading-tight">
