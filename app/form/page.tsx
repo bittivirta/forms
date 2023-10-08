a78931f32dce580c105564d61158ca64a816d817
Mikko Salonen <mikko.salonen@tuni.fi>
2023-10-08 12:56:36 +0300

    Form page now supports multiple forms. Fetching different forms based on the url path.

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 
+import { useSearchParams } from "next/navigation";
+
 import Header from "../components/ui/header";
 import Footer from "../components/ui/footer";
 
@@ -16,20 +18,25 @@ interface BivForm {
   title: string;
   description: string;
   fields: FormField[];
+  error: string;
 }
 
-async function fetchForm(): Promise<BivForm> {
-  const response = await fetch("/api/forms?id=1");
+async function fetchForm(id): Promise<BivForm> {
+  const formurl = "/api/forms?id=" + id;
+  const response = await fetch(formurl);
   const json = await response.json();
   return json;
 }
 
 export default function Form() {
+  const searchParams = useSearchParams();
+
   const [form, setForm] = useState<BivForm | null>(null);
+  const reqId = searchParams.get("id");
 
   useEffect(() => {
     async function loadForm() {
-      const form = await fetchForm();
+      const form = await fetchForm(reqId);
       setForm(form);
     }
 
@@ -39,15 +46,35 @@ export default function Form() {
   if (!form) {
     return null;
   }
+  //if fetched form contains {"error":"No data found"} then return 404 page
+  if (form.error || form.fields.length === 0) {
+    return (
+      <main className="dark:bg-primary-900">
+        <Header />
+        <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-12 lg:py-16">
+          <div className="mx-auto max-w-2xl text-center">
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-100 md:text-5xl lg:text-6xl">
+              404 - Requested form not found
+            </h1>
+            <p className="text-xl text-gray-600 dark:text-gray-200">
+              Please check the URL or if you think this is an error, please
+              contact us at on Discord.
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </main>
+    );
+  }
   return (
-    <main>
+    <main className="dark:bg-primary-900">
       <Header />
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-12 lg:py-16">
         <div className="mx-auto max-w-2xl text-center">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-100 md:text-5xl lg:text-6xl">
             {form.title}
           </h1>
-          <p className="text-xl text-gray-600 dark:text-gray-400">
+          <p className="text-xl text-gray-600 dark:text-gray-200">
             {form.description}
           </p>
         </div>
@@ -57,7 +84,7 @@ export default function Form() {
               <div className="p-4">
                 <label
                   htmlFor={field.id}
-                  className="block text-primary-600 dark:text-primary-300"
+                  className="block text-primary-600 dark:text-gray-200"
                 >
                   {field.label}
                 </label>
@@ -66,7 +93,7 @@ export default function Form() {
                   type={field.type}
                   placeholder={field.placeholder}
                   required={field.required}
-                  className="w-full p-2 mt-2 rounded-lg bg-primary-200 dark:bg-primary-300 text-gray-600 border-2 border-indigo-300 dark:bg-primary-800 dark:text-dark-300"
+                  className="w-full p-2 mt-2 rounded-lg bg-primary-200 dark:bg-gray-700 text-gray-600 border-2 border-indigo-300 dark:bg-primary-800 dark:text-dark-300 {field.required ? 'border-red-500' : 'border-gray-300'}"
                 />
               </div>
             </form>
