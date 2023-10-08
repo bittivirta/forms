9af2f83516a34eea4be9254f51286d2dae0485bf
Mikko Salonen <mikko.salonen@tuni.fi>
2023-10-08 12:24:21 +0300

    Form page fetches data url and shows it when page has loaded.

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 
 import Header from "../components/ui/header";
 import Footer from "../components/ui/footer";
@@ -17,53 +18,62 @@ interface BivForm {
   fields: FormField[];
 }
 
-const form: BivForm = {
-  title: 'Example Form',
-  description: 'This is an example form.',
-  fields: [
-    { id: 'name', type: 'text', label: 'Name', placeholder: 'Your name', required: true },
-    { id: 'email', type: 'email', label: 'Email', placeholder: 'Your email', required: true },
-    { id: 'phone', type: 'tel', label: 'Phone', placeholder: 'Your phone number', required: false },
-    { id: 'message', type: 'textarea', label: 'Message', placeholder: 'Your message', required: true },
-    { id: 'submit', type: 'submit', label: 'Submit', placeholder: 'Submit', required: true }
-  ]
-};
-function ShowForm(jsonval: BivForm) {
-  // parse json and create form with included fields.
-  return (
-    <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-      <div className="mx-auto max-w-2xl">
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          {jsonval.title}
-        </h1>
-        <p className="text-xl text-gray-600 dark:text-gray-400">
-          {jsonval.description}
-        </p>
-      </div>
-      <div>
-        {jsonval.fields.map((field, i) => (
-          <form key={i}>
-          <div className="rounded-lg bg-primary-100 p-4 text-primary-600 dark:bg-primary-900 dark:text-primary-300">
-            <h2 className="text-3xl font-extrabold leading-tight">
-              {field.label}
-            </h2>
-            <input type={field.type} placeholder={field.placeholder} required={field.required} />
-
-          </div></form>
-        ))}
-      </div>
-    </div>
-  );
-
+async function fetchForm(): Promise<BivForm> {
+  const response = await fetch("/api/forms?id=1");
+  const json = await response.json();
+  return json;
 }
-export default function forms() {
+
+export default function Form() {
+  const [form, setForm] = useState<BivForm | null>(null);
+
+  useEffect(() => {
+    async function loadForm() {
+      const form = await fetchForm();
+      setForm(form);
+    }
+
+    loadForm();
+  }, []);
+
+  if (!form) {
+    return null;
+  }
   return (
     <main>
       <Header />
-      <section className="bg-white dark:bg-gray-900">
-      {ShowForm(form)}
-      </section>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-12 lg:py-16">
+        <div className="mx-auto max-w-2xl text-center">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+            {form.title}
+          </h1>
+          <p className="text-xl text-gray-600 dark:text-gray-400">
+            {form.description}
+          </p>
+        </div>
+        <div>
+          {form.fields.map((field, i) => (
+            <form key={i}>
+              <div className="p-4">
+                <label
+                  htmlFor={field.id}
+                  className="block text-primary-600 dark:text-primary-300"
+                >
+                  {field.label}
+                </label>
+
+                <input
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  required={field.required}
+                  className="w-full p-2 mt-2 rounded-lg bg-primary-200 dark:bg-primary-300 text-gray-600 border-2 border-indigo-300 dark:bg-primary-800 dark:text-dark-300"
+                />
+              </div>
+            </form>
+          ))}
+        </div>
+      </div>
       <Footer />
     </main>
-    );
+  );
 }
