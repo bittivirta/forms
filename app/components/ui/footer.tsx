02386c4a5713aa30a35e45d8d1ac4b7d1c370a66
Mikko Salonen <mikko.salonen@tuni.fi>
2023-11-06 23:29:51 +0200

    Fixed Bittivirta text visibility

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -8,18 +8,37 @@ export default function Footer() {
       <div className="lg:-10 mx-auto max-w-screen-xl p-4 py-6 md:p-8">
         <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
           <div className="col-span-3">
-            <a href="#" className="mb-2 flex items-center text-2xl font-semibold text-gray-900 dark:text-white sm:mb-0">
-              <Image alt="Bittivirta Logo" src="https://cdn.bittivirta.fi/graphics/logo/2023/bittivirta/svg/logo.svg" width={130} height={50} />
+            <a
+              href="#"
+              className="mb-2 flex items-center text-2xl font-semibold text-gray-900 dark:text-white sm:mb-0"
+            >
+              <Image
+                alt="Bittivirta Logo"
+                src="https://cdn.bittivirta.fi/graphics/logo/2023/bittivirta/svg/icon.svg"
+                className="mr-3"
+                width={50}
+                height={50}
+              />
+              <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                Bittivirta
+              </span>
             </a>
             <p className="my-4 font-light text-gray-500 dark:text-gray-400">
-              Bittivirta provides a variety of IT services, including maintenance, planning, repairs, and more. We offer exceptional quality customer service, affordable prices without sacrificing
-              quality, and user-friendly services. Contact us today to find out how we can streamline your IT needs.
+              Bittivirta provides a variety of IT services, including
+              maintenance, planning, repairs, and more. We offer exceptional
+              quality customer service, affordable prices without sacrificing
+              quality, and user-friendly services. Contact us today to find out
+              how we can streamline your IT needs.
             </p>
 
-            <p className="my-4 text-xs font-bold text-gray-500 dark:text-gray-400">CUSTOMER ORIENTATION - QUALITY - SIMPLICITY</p>
+            <p className="my-4 text-xs font-bold text-gray-500 dark:text-gray-400">
+              CUSTOMER ORIENTATION - QUALITY - SIMPLICITY
+            </p>
           </div>
           <div className="lg:mx-auto">
-            <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Company</h2>
+            <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+              Company
+            </h2>
             <ul className="text-gray-500 dark:text-gray-400">
               <li className="mb-4">
                 <a href="#" className=" hover:underline">
@@ -44,7 +63,9 @@ export default function Footer() {
             </ul>
           </div>
           <div className="lg:mx-auto">
-            <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Help center</h2>
+            <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+              Help center
+            </h2>
             <ul className="text-gray-500 dark:text-gray-400">
               <li className="mb-4">
                 <a href="#" className="hover:underline">
@@ -69,7 +90,9 @@ export default function Footer() {
             </ul>
           </div>
           <div className="lg:mx-auto">
-            <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Legal</h2>
+            <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+              Legal
+            </h2>
             <ul className="text-gray-500 dark:text-gray-400">
               <li className="mb-4">
                 <a href="#" className="hover:underline">
