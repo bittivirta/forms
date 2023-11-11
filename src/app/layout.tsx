43700115a5ec320062713d9294a71f8237ab9d43
Mikko Salonen <mikko.salonen@tuni.fi>
2023-11-11 20:50:07 +0200

    Updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import "./scss/global.scss";
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "/dist/output.css";
+config.autoAddCss = false;
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
-  title: "Next.js + TypeScript + Tailwind + Flowbite + SCSS = Bittivirta 23 Boilerplate",
+  title:
+    "Next.js + TypeScript + Tailwind + Flowbite + SCSS = Bittivirta 23 Boilerplate",
   description: "Make a wish, and we will play it for you.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={ubuntu.className}>{children}</body>
