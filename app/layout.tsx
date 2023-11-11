a22c1dab7f8a31ef304d1f6134756b106589ddfd
Mikko Salonen <mikko.salonen@tuni.fi>
2023-11-11 16:02:08 +0200

    Updated layout texts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
 import "./scss/global.scss";
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Bittivirta Forms",
-  description: "Make a wish, and we will play it for you.",
+  description: "Bittivirta Forms, a moden form provider for the modern web",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={ubuntu.className}>{children}</body>
