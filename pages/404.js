dd79b914c2aac99e458e3465c93bcc210cff0dc8
Mikko Salonen <mikko.salonen@tuni.fi>
2023-11-13 13:54:19 +0200

    Updated static page text colours on light enviroment.

--- pages/404.js
+++ pages/404.js
@@ -13,10 +13,10 @@ export default function Custom404() {
       <Header />
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-12 lg:py-16">
         <div className="mx-auto max-w-2xl text-center">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-gray-100 md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl lg:text-6xl">
             404
           </h1>
-          <p className="text-3xl text-gray-600 dark:text-gray-200 mb-4">
+          <p className="text-3xl text-gray-200 dark:text-gray-300 mb-4">
             Please check the URL or if you think this is an error, please
             contact us.
           </p>
